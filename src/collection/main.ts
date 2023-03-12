66f8a6cb1b7ac4f8fdaafcd2323dbc6ee16f6026
alu0101327679 <alu0101327679@ull.edu.es>
2023-03-12 20:10:19 +0000

    incorporados los archivos de collection

--- src/collection/main.ts
+++ src/collection/main.ts
@@ -5,25 +5,25 @@ import { Discografy } from "./discografy";
 import { Group } from "./group";
 import { ArtistCollection } from "./collection";
 
-const cancion_base = new Cancion("cacatua", 366, ["country", "rock"], true, 6);
-const cancion_dos = new Cancion("selfmade", 366, ["country", "rock"], true, 2);
+// const cancion_base = new Cancion("cacatua", 366, ["country", "rock"], true, 6);
+// const cancion_dos = new Cancion("selfmade", 366, ["country", "rock"], true, 2);
 
-const CD_base = new CD("disco", 1932, [cancion_base]);
-const CD_dos = new CD("segundo", 1935, [cancion_dos]);
-const discografia_base = new Discografy([CD_base, CD_dos]);
-const discografia_bdos = new Discografy([CD_dos]);
+// const CD_base = new CD("disco", 1932, [cancion_base]);
+// const CD_dos = new CD("segundo", 1935, [cancion_dos]);
+// const discografia_base = new Discografy([CD_base, CD_dos]);
+// const discografia_bdos = new Discografy([CD_dos]);
 
-const individual = new Single("hernesto", 35, discografia_base);
-const grupo_de_musica = new Group(
-  "javier",
-  6,
-  discografia_bdos,
-  "casi a cenar"
-);
+// const individual = new Single("hernesto", 35, discografia_base);
+// const grupo_de_musica = new Group(
+//   "javier",
+//   6,
+//   discografia_bdos,
+//   "casi a cenar"
+// );
 
-const myCollection = new ArtistCollection([individual, grupo_de_musica]);
+// const myCollection = new ArtistCollection([individual, grupo_de_musica]);
 
-let variable: boolean = myCollection.searchCD("disco");
+// let variable: boolean = myCollection.searchCD("disco");
 // console.log(variable);
 // console.log(CD_base)
 
